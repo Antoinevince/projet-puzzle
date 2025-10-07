bcd578484b18982d475d7084f1459e429b0895a3
Antoinevince <antoinevincent318@icloud.com>
2025-10-07 17:55:08 +0200

    fignollage

---
+++ code-project-puzzle/test.py
@@ -0,0 +1,18 @@
+import coordinates
+
+# Configuration du cube résolu
+coins =[
+    [0, 0], [1, 0], [2, 0], [3, 0],  # Coins 0 à 3
+    [4, 0], [5, 0], [6, 0], [7, 0]   # Coins 4 à 7
+]
+
+arretes = [
+    [0, 0], [1, 0], [2, 0], [3, 0],  # Arêtes 0 à 3
+    [4, 0], [5, 0], [6, 0], [7, 0],  # Arêtes 4 à 7
+    [8, 0], [9, 0], [10, 0], [11, 0] # Arêtes 8 à 11
+]
+state = [coins, arretes]
+
+dummy_config = coordinates.ClassicRubiksCube(state)
+
+coordinates.ClassicRubiksCube.rotateF(dummy_config)
