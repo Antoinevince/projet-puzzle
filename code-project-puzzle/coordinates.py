0367d6a22c418280b353b5c106c04bc2efa18cba
Antoinevince <antoinevincent318@icloud.com>
2025-10-07 16:56:25 +0200

    ajout du dossier images

--- code-project-puzzle/coordinates.py
+++ code-project-puzzle/coordinates.py
@@ -2,6 +2,7 @@
 ce fichier sert à implémenter le système de coordonnées du rubik's cube, de la quarter turn metric
 """
 
+import copy
 
 
 """on va représenter les configurations du cube sous forme de tableaux qui contiennent les mouvements qui ont permis d'arriver à cette configuration"""
@@ -9,96 +10,43 @@ ce fichier sert à implémenter le système de coordonnées du rubik's cube, de 
 #rubik's cube 3x3x3
 class ClassicRubiksCube():
 
+
+#on implémente les coordonnées en notant les coins et leur orientation sous formes de listes
+
+#chaque coin sera représenté par une liste contenant la position du coin et son orientation
+
+
     def __init__(self, state):
-        self.state = state
+        self.coins = state[0]
+        self.arretes = state[1]
     
 #les fonctions suivantes servent à gérer les différents mouvements que l'on peut faire sur les cube, et les interactions (ie si on fait un mouvement a, puis on effectue le mouvement inverse, il faut donc actualiser l'état du cube)
     
     def rotateF(self):
+        self.new_coins = copy.deepcopy(self.coins) #copie de la liste des coords des coins
 
-        if self.state[-1] != 'Fi':
-            self.state.append('F')
-        else: 
-            self.state= self.state.pop()
+        #permutations des positions des 
+        self.new_arretes = copy.deepcopy(self.arretes)  #copie de la liste des coords des coins
 
-        if self.state[-1] == 'F' and self.state[-2] == 'F' and self.state[-3] == 'F':
-            self.state = self.state.pop()
-            self.state = self.state.pop()
-            self.state = self.state.pop()
-            self.state.append('Fi')
-        
-        return self.state
+
 
     def rotateR(self):
 
-        if self.state[-1] != 'Ri':
-            self.state.append('R')
-        else: 
-            self.state= self.state.pop()
-
-        if self.state[-1] == 'R' and self.state[-2] == 'R' and self.state[-3] == 'R':
-            self.state = self.state.pop()
-            self.state = self.state.pop()
-            self.state = self.state.pop()
-            self.state.append('Ri')
-        
-        return self.state
+        pass
 
     def rotateU(self):
-        if self.state[-1] != 'Ui':
-            self.state.append('U')
-        else: 
-            self.state= self.state.pop()
-
-        if self.state[-1] == 'U' and self.state[-2] == 'U' and self.state[-3] == 'U':
-            self.state = self.state.pop()
-            self.state = self.state.pop()
-            self.state = self.state.pop()
-            self.state.append('Ui')
-        
-        return self.state
+        pass
 
     def rotateL(self):
-        if self.state[-1] != 'Li':
-            self.state.append('L')
-        else: 
-            self.state= self.state.pop()
-
-        if self.state[-1] == 'L' and self.state[-2] == 'L' and self.state[-3] == 'L':
-            self.state = self.state.pop()
-            self.state = self.state.pop()
-            self.state = self.state.pop()
-            self.state.append('Li')
+        pass
         
         return self.state
 
     def rotateB(self):
-        if self.state[-1] != 'Bi':
-            self.state.append('B')
-        else: 
-            self.state= self.state.pop()
-
-        if self.state[-1] == 'B' and self.state[-2] == 'B' and self.state[-3] == 'B':
-            self.state = self.state.pop()
-            self.state = self.state.pop()
-            self.state = self.state.pop()
-            self.state.append('Bi')
-        
-        return self.state
+       pass
 
     def rotateD(self):
-        if self.state[-1] != 'Di':
-            self.state.append('D')
-        else: 
-            self.state= self.state.pop()
-
-        if self.state[-1] == 'D' and self.state[-2] == 'D' and self.state[-3] == 'D':
-            self.state = self.state.pop()
-            self.state = self.state.pop()
-            self.state = self.state.pop()
-            self.state.append('Di')
-        
-        return self.state
+        pass
 
 
 #cependant, à cause de la lourdeur en mémoire de ce type de représentation, nous allons en utiliser d'autres
